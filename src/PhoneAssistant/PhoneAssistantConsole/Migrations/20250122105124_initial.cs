8802054861c601fd647dcca518239aac9ccbf4d9
Alexander <sany199574@gmail.com>
2025-01-22 13:57:59 +0300

    Исправлены проблемы с миграцией

--- src/PhoneAssistant/PhoneAssistantConsole/Migrations/20250122105124_initial.cs
+++ src/PhoneAssistant/PhoneAssistantConsole/Migrations/20250122105124_initial.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace PhoneAssistantConsole.Migrations
 {
     /// <inheritdoc />
-    public partial class Initials : Migration
+    public partial class initial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -14,8 +15,7 @@ namespace PhoneAssistantConsole.Migrations
                 name: "Contacts",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
                     Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
                 },
