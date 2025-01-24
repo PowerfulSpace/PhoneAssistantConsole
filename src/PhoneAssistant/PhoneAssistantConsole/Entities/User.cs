4de94571ad4cf9c5be9c484f8e32bc3e044e6e95
Alexander <sany199574@gmail.com>
2025-01-24 19:05:13 +0300

    Добавлен класс User и файл CONTRIBUTING.md

---
+++ src/PhoneAssistant/PhoneAssistantConsole/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneAssistantConsole.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+    }
+}
