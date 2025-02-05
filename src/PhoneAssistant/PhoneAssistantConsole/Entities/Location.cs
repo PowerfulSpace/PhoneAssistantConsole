9139446a069ecb38c80c43807590c121c104749c
Alexander <sany199574@gmail.com>
2025-02-05 06:03:07 +0300

    Добавлено свойство  - Longitude

--- src/PhoneAssistant/PhoneAssistantConsole/Entities/Location.cs
+++ src/PhoneAssistant/PhoneAssistantConsole/Entities/Location.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string Latitude { get; set; }
+        public string Longitude { get; set; }
     }
 }
