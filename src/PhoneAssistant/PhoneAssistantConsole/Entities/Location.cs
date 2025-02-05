fbd1b4eae490a341658f2c7cb41a9460321e880c
Alexander <sany199574@gmail.com>
2025-02-05 06:03:59 +0300

    Добавлено свойство  - Region

--- src/PhoneAssistant/PhoneAssistantConsole/Entities/Location.cs
+++ src/PhoneAssistant/PhoneAssistantConsole/Entities/Location.cs
@@ -8,5 +8,6 @@
         public string Latitude { get; set; }
         public string Longitude { get; set; }
         public string Country { get; set; }
+        public string Region { get; set; }
     }
 }
